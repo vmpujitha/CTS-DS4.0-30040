08739eecb009ef865e9490fd6bf8c7a6780b4ed9
vmpujitha <147124471+vmpujitha@users.noreply.github.com>
2025-06-19 21:13:14 +0530

    Add files via upload

---
+++ FactoryPatternExample/src/factorymethod/DocumentFactory.java
@@ -0,0 +1,5 @@
+package factorymethod;
+
+public abstract class DocumentFactory {
+    public abstract Document createDocument();
+}
---
+++ FactoryPatternExample/src/factorymethod/ExcelDocumentFactory.java
@@ -0,0 +1,8 @@
+package factorymethod;
+
+public class ExcelDocumentFactory extends DocumentFactory {
+    @Override
+    public Document createDocument() {
+        return new ExcelDocument();
+    }
+}
