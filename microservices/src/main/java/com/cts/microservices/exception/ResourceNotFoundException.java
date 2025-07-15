4d8ab39ce38c9b803513747fe93d1c774dd6686c
vmpujitha <147124471+vmpujitha@users.noreply.github.com>
2025-07-15 22:10:57 +0530

    Delete microservices directory

--- microservices/src/main/java/com/cts/microservices/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.cts.microservices.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
